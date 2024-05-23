bab519ec05e2d6516801f550e8ec5018a774e81d
ZanyaFR <zanya1996@gmail.com>
2024-05-23 09:01:52 +0200

    saving changes : working on populating DB

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PaymentSystem;
+using PaymentSystem.DAL.Models;
+using PaymentSystem.Models;
 
 namespace TestConsoleApp
 {
@@ -9,11 +11,33 @@ namespace TestConsoleApp
         {
             var context = new SystemContext();
 
+            var created = context.Database.EnsureCreated();
+            if (created)
+            {
+                // Add data into Database
+                seed();
+            }
+
             context.Database.Migrate();
 
             Console.Write("Done");
 
 
+
+        }
+
+        private static void seed()
+        {
+            var context = new SystemContext();
+
+            var student1 = new Student() { Firstname = "Danny", Lastname = "Smith", Username = "DannySmith" };
+            var student2 = new Student() { Firstname = "John", Lastname = "Doe", Username = "JohnDoe" };
+            context.Students.AddRange(student1, student2);
+
+            var account1 = new Account() { Student = student1, Username = "DannySmith", Balance = 1000, IsActive = true };
+            var account2 = new Account() { Student = student2, Username = "JohnDoe", Balance = 2000, IsActive = true };
+            context.Accounts.AddRange(account1, account2);
+
+
         }
     }
-}
